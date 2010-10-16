532ba5bb1533c599ca19936826bd652a3c0929f6
Enoch Root <enoch@ubuntu.localdomain>
2010-10-16 07:52:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iep::Application.config.secret_token = '1907205d0158ef51871fe15a1a1968db7161f6b35aed5a5333902202f437e205c807ed1e8c6b837b29900d286655c6dbd36fc72f62ff3a1b8015072f135abc16'
